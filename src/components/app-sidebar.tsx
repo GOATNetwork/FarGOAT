4176af680209c93425e7b7d19184524bc94ff6bc
AYUSH SINGH <ayush@AYUSHs-MacBook-Air.local>
2024-11-12 03:17:56 +0700

    support page

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -76,12 +76,12 @@ const data = {
     navSecondary: [
         {
             title: 'Support',
-            url: '#',
+            url: '/ecosystem/support',
             icon: LifeBuoy,
         },
         {
             title: 'Feedback',
-            url: '#',
+            url: '#', 
             icon: Send,
         },
     ],
